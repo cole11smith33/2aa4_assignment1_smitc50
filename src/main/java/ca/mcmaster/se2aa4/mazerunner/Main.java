c059fff44ab430074c39116c1e70e23a6770883b
cole11smith33 <cole11smith33@gmail.com>
2025-04-04 11:47:00 -0400

    implemented builder pattern (still needs comments)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -13,13 +13,95 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.w3c.dom.UserDataHandler;
 
-public class Main {
+import ca.mcmaster.se2aa4.mazerunner.*;
 
+public class Main{
+    private final String filepath;
+    private final String pathway;
+    private final Maze maze;
+    private final Player player1;
+    private final PathfindingAlgorithm algorithm;
     private static final Logger logger = LogManager.getLogger();
 
-    public static void main(String[] args) {
+    public Main (MazerunnerBuilder builder) {
+        this.filepath = builder.filepath;
+        this.pathway = builder.pathway;
+        this.maze = builder.maze;
+        this.player1 = builder.player1;
+        this.algorithm = builder.algorithm;
+    }
+
+    public static class MazerunnerBuilder{   
+        private String filepath;
+        private String pathway;
+        private Maze maze;
+        private Player player1;
+        private PathfindingAlgorithm algorithm;
         
+        public MazerunnerBuilder(String filepath) {
+            this.filepath = filepath;
+        }
+
+        public MazerunnerBuilder setPathway(String pathway) {
+            this.pathway = pathway;
+            return this;
+        }
+
+        public MazerunnerBuilder setMaze(Maze maze) {
+            this.maze = maze;
+            return this;
+        }
+
+        public MazerunnerBuilder setPlayer(Player player1) {
+            this.player1 = player1;
+            return this;
+        }
+
+        public MazerunnerBuilder setAlgorithm(PathfindingAlgorithm algorithm) {
+            this.algorithm = algorithm;
+            return this;
+        }
+        public Main build() {
+            return new Main(this);
+        }
+    }
+
+    private void run () {
+
+            try {
+                logger.info("**** Reading the maze from file " + filepath);
+                BufferedReader reader = new BufferedReader(new FileReader(filepath));
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    for (int idx = 0; idx < line.length(); idx++) {
+
+                        if (line.charAt(idx) == '#') {
+                            logger.trace("WALL ");
+                        }
+                        else if (line.charAt(idx) == ' ') {
+                            logger.trace("PASS ");
+                        }
+                    }
+
+                    logger.trace(System.lineSeparator());
+                }
+
+            } catch(Exception e) {
+                logger.error("/!\\ An error has occured /!\\");
+            }
+
+            player1.placePlayer(maze.getMaze()); //place player in maze            
+            ConvertPath pathfinder = new ConvertPath(algorithm);
+            pathfinder.pathfind(player1, maze); //find or validate the path depending on if -p is present (Liskov Substitution Principle shown here)
+    }
+
+
+    public static void main(String[] args) {
+
+                        
         Options options = new Options();
+
         Option flag = Option.builder("i")
             .hasArg()
             .required() //requires that a argument be given
@@ -33,51 +115,34 @@ public class Main {
 
         options.addOption(flag); // add filepath flag option
         options.addOption(pFlag); // add path flag option
+        
         CommandLineParser parser = new DefaultParser();
 
         try {
             CommandLine commandLine = parser.parse(options, args); //parse command line
             logger.info("** Starting Maze Runner");
+
             String filepath = commandLine.getOptionValue("i");
-
-
-            try {
-                logger.info("**** Reading the maze from file " + filepath);
-                BufferedReader reader = new BufferedReader(new FileReader(filepath));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    for (int idx = 0; idx < line.length(); idx++) {
-                        if (line.charAt(idx) == '#') {
-                            logger.trace("WALL ");
-                        } else if (line.charAt(idx) == ' ') {
-                            logger.trace("PASS ");
-                        }
-                    }
-                    logger.trace(System.lineSeparator());
-                }
-            } catch(Exception e) {
-                logger.error("/!\\ An error has occured /!\\");
-            }
-
-
-
-            Maze maze = new Maze(filepath);
-            Player player1 = new Player(maze.findEntrance());
-            ConvertPath pathfinder;
-
-            player1.placePlayer(maze.getMaze()); //place player in maze
+            String pathway = commandLine.getOptionValue("p");
 
             PathfindingAlgorithm algorithm;
-
             if (commandLine.hasOption("p")){ //if path is given, then use the UserProvidedPath algorithm
-                String pathway = commandLine.getOptionValue("p");
                 algorithm = new UserProvidedPath(pathway);
             }
             else { //if the path isn't given, the use the RightHandRule algorithm
                 algorithm = new RightHandRule();
-            }    
-            pathfinder = new ConvertPath(algorithm);
-            pathfinder.pathfind(player1, maze); //find or validate the path depending on if -p is present (Liskov Substitution Principle shown here)
+            }
+
+            Maze maze = new Maze(filepath); //create maze object
+            Player player1 = new Player(maze.findEntrance()); //create player object
+
+            Main mazeRunner = new MazerunnerBuilder(filepath) //mazerunner builder
+                .setPathway(pathway)
+                .setMaze(maze)
+                .setPlayer(player1)
+                .setAlgorithm(algorithm)
+                .build();
+            mazeRunner.run(); //run the maze runner
 
         } catch (ParseException e) {
             logger.error(e.getMessage());
@@ -85,3 +150,4 @@ public class Main {
         logger.info("** End of MazeRunner");
     }
 }
+
